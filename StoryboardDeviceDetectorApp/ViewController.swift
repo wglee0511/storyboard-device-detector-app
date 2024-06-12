c88d293eb9a541579f662a392abe8882e5aa9c63
wglee0511 <ranazang@naver.com>
2024-06-12 19:38:49 +0900

    Initial Commit

---
+++ StoryboardDeviceDetectorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardDeviceDetectorApp
+//
+//  Created by racoon on 6/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
